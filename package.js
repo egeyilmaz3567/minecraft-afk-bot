1d0972deea295a5ce8ae9729a2f742e7918f4446
egeyilmaz3567 <egeyilmaz3567@gmail.com>
2026-09-07 13:47:06 +0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,11 @@
+{
+  "name": "minecraft-afk-bot",
+  "version": "1.0.0",
+  "main": "bott.js",
+  "scripts": {
+    "start": "node bott.js"
+  },
+  "dependencies": {
+    "mineflayer": "^4.25.0"
+  }
+}
